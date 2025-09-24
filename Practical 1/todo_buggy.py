928f4fce1f77248481a5d1ff4c2845a0f9f1a3a9
Nikita <97588942+7niksol@users.noreply.github.com>
2025-09-24 16:51:09 +0300

    Test conflict
    Conflict escalation with test branch

--- Practical 1/todo_buggy.py
+++ Practical 1/todo_buggy.py
@@ -13,6 +13,8 @@ To-Do List (intentionally buggy) — Практична робота №1
 Тестова зміна для перевірки git
 """
 
+print("Тестова зміна для перевірки конфліктів git та злиття.") 
+
 class TodoApp:
     def __init__(self, root):
         self.root = root
